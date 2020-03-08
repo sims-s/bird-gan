b56c233830799660dd4eb0b18d53585b8945472e
Sam <samhsims@gmail.com>
2020-03-08 15:52:03 -0400

    stylegan forwward pass working

--- src/blocks_and_layers.py
+++ src/blocks_and_layers.py
@@ -4,7 +4,78 @@ import torch.nn.functional as F
 import numpy as np
 
 
+
 '''===================================General Layers==============================================='''
+class EqualizedConv2dUp(nn.Module):
+    def __init__(self, channels_in, channels_out, kernel_size, stride=1, intermediate=None):
+        super(EqualizedConv2dUp, self).__init__()
+        self.upscale = Upscale2d()
+        self.kernel_size = kernel_size if isinstance(kernel_size, tuple) else (kernel_size, kernel_size)
+        self.scale_by = np.sqrt(2 / (self.kernel_size[0]*self.kernel_size[1]*channels_in))
+
+        self.weight = nn.Parameter(torch.randn(channels_out, channels_in, self.kernel_size[0], self.kernel_size[1]))
+        self.bias = nn.Parameter(torch.zeros(channels_out))
+        self.intermediate = intermediate
+
+    def forward(self, x):
+        if min(x.shape[2:]) >= 64:
+            scaled_weights = self.weight * self.scale_by
+            scaled_weights = scaled_weights.permute(1,0,2,3)
+            scaled_weights = F.pad(scaled_weights, [1,1,1,1])
+            # Other persons code doesn't scale by .25... seems like we should?
+            scaled_weights = (scaled_weights[:, :, 1:, 1:] + scaled_weights[:, :, :-1, 1:] + \
+                            scaled_weights[:, :, 1:, :-1] + scaled_weights[:, :, :-1, :-1]) * .25
+            x = F.conv_transpose2d(x, scaled_weights, stride=2, padding=(self.weight.size(-1)-1)//2)
+
+        else:
+            x = self.upscale(x)
+            x = F.conv2d(x, self.weight * self.scale_by, None, padding = self.kernel_size[0]//2)
+        
+        if self.intermediate is not None:
+            x = self.intermediate(x)
+        x = x + self.bias.view(1, -1, 1, 1)
+        return x
+
+class EqualizedConv2dDown(nn.Module):
+    def __init__(self, channels_in, channels_out, kernel_size, stride=1):
+        super(EqualizedConv2dDown, self).__init__()
+        self.downscale = Downscale2d()
+        self.kernel_size = kernel_size if isinstance(kernel_size, tuple) else (kernel_size, kernel_size)
+        self.scale_by = np.sqrt(2 / (self.kernel_size[0]*self.kernel_size[1]*channels_in))
+        
+        self.weight = nn.Parameter(torch.randn(channels_out, channels_in, self.kernel_size[0], self.kernel_size[1]))
+        self.bias = nn.Parameter(torch.zeros(channels_out))
+        
+
+    def forward(self, x):
+        # why are these different size conditions?
+        if min(x.shape[2:]) >= 64:
+            scaled_weights = self.weight * self.scale_by
+            scaled_weights = F.pad(scaled_weights, [1,1,1,1])
+            scaled_weights = (scaled_weights[:, :, 1:, 1:] + scaled_weights[:, :, :-1, 1:] + \
+                              scaled_weights[:, :, 1:, :-1] + scaled_weights[:, :, :-1, :-1]) * .25
+            x = F.conv2d(x, scaled_weights, stride=2, padding=(scaled_weights.size(-1)-1)//2)
+        else:
+            x = F.conv2d(x, self.weight*self.scale_by, None, padding=self.kernel_size[0]//2)
+            x = self.downscale(x)
+        x = x + self.bias.view(1, -1, 1, 1)
+        return x
+
+
+class PerChannelNoise(nn.Module):
+    def __init__(self, channels):
+        super(PerChannelNoise, self).__init__()
+        self.weight = nn.Parameter(torch.zeros(channels))
+
+    def forward(self, x, noise=None):
+        if noise is None:
+            noise = torch.randn(x.size(0), 1, x.size(2), x.size(3))
+        else:
+            if not hasattr(self, 'noise'):
+                raise ValueError('Need to either pass or set noise!')
+            nosie = self.noise
+
+        return x + self.weight.view(1, -1, 1, 1) * noise
 
 
 
@@ -13,7 +84,7 @@ class EqualizedLRConv2d(nn.Module):
         super(EqualizedLRConv2d, self).__init__()
         
         kernel_size = kernel_size if isinstance(kernel_size, tuple) else (kernel_size, kernel_size)
-        self.weight = nn.Parameter(nn.init.normal_(torch.empty(channels_out, channels_in, kernel_size[0], kernel_size[1])))
+        self.weight = nn.Parameter(torch.randn(channels_out, channels_in, kernel_size[0], kernel_size[1]))
         
         self.stride = stride
         self.padding = padding
@@ -32,12 +103,12 @@ class EqualizedLRConv2dTranspose(nn.Module):
     def __init__(self, channels_in, channels_out, kernel_size, stride=1, padding=0):
         super(EqualizedLRConv2dTranspose, self).__init__()
         kernel_size = kernel_size if isinstance(kernel_size, tuple) else (kernel_size, kernel_size)
-        self.weight = nn.Parameter(nn.init.normal_(torch.empty(channels_in, channels_out, kernel_size[0], kernel_size[1])))
+        self.weight = nn.Parameter(torch.randn(channels_in, channels_out, kernel_size[0], kernel_size[1]))
         
         self.stride = stride
         self.padding = padding
         
-        self.bias = nn.Parameter(torch.FloatTensor(channels_out).fill_(0))
+        self.bias = nn.Parameter(torch.zeros(channels_out).fill_(0))
             
         self.scale_by = np.sqrt(2. / channels_in)
         
@@ -49,12 +120,10 @@ class EqualizedLRConv2dTranspose(nn.Module):
 class EqualizedLRLinear(nn.Module):
     def __init__(self, dim_in, dim_out):
         super(EqualizedLRLinear, self).__init__()
-        self.weight = nn.Parameter(nn.init.normal_(nn.empty(dim_in, dim_out)))
+        self.scale_by = np.sqrt(2./dim_in)
+        self.weight = nn.Parameter(torch.randn(dim_out, dim_in))
+        self.bias = nn.Parameter(torch.zeros(dim_out))   
         
-
-        self.bias = nn.Parameter(torch.FloatTensor(dim_out).fill_(0))
-            
-        self.scale_by = sqrt(2./dim_in)
         
     def forward(self, x):
         return F.linear(x, self.weight * self.scale_by, self.bias)
@@ -65,8 +134,8 @@ class PixelNorm(nn.Module):
         super(PixelNorm, self).__init__()
         
     def forward(self, x, eps=10**-8):
-        norm = (x.pow(2).mean(dim=1,keepdim=True) + eps).sqrt()
-        return x/norm
+        norm = x.pow(2).mean(dim=1,keepdim=True) + eps
+        return x * torch.rsqrt(norm)
 
 class AddStandardDeviation(nn.Module):
     def __init__(self):
@@ -78,9 +147,149 @@ class AddStandardDeviation(nn.Module):
         stds = stds.repeat(batch_size, 1, x.size()[2], x.size()[3])
         return torch.cat([x, stds], 1)
 
+class AddStandardDeviationGrouped(nn.Module):
+    def __init__(self, group_size=4):
+        super(AddStandardDeviationGrouped, self).__init__()
+        self.group_size = group_size
 
-'''===================================Progan Layers==============================================='''
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+        group_size = min(self.group_size, batch_size)
+        y = x.reshape([group_size, -1, 1, channels, height, width])
+        y = y - y.mean(0, keepdim=True)
+        y = ((y**2).mean(0, keepdim=True) + 10**-8).sqrt()
+        y = y.mean([3,4,5], keepdim=True).squeeze(3)
+        y = y.expand(group_size, -1, -1, height, width).clone().reshape(batch_size, 1, height, width)
+        return torch.cat([x, y], dim=1)
 
+
+class StyleMod(nn.Module):
+    def __init__(self, nb_channels, latent_size):
+        super(StyleMod, self).__init__()
+        self.layer = EqualizedLRLinear(latent_size, nb_channels*2)
+
+    def forward(self, x, latent):
+        style = self.layer(latent)
+        style = style.view(-1, 2, x.size(1), 1, 1)
+        x = x + x * style[:,0] + style[:,1]
+        return x
+
+class NoiseLReluNormStyle(nn.Module):
+    def __init__(self, nb_channels, latent_size):
+        super(NoiseLReluNormStyle, self).__init__()
+        self.layers = nn.Sequential(
+            PerChannelNoise(nb_channels),
+            nn.LeakyReLU(.2),
+            nn.InstanceNorm2d(nb_channels)
+        )
+
+        self.style_mod = StyleMod(nb_channels, latent_size)
+
+    def forward(self, x, other_style):
+        x = self.layers(x)
+        x = self.style_mod(x, other_style)
+        return x
+
+class Truncation(nn.Module):
+    def __init__(self, avg_latent, max_layer=8, thresh=.7, ema_coeff=.995):
+        super(Truncation, self).__init__()
+        self.max_layer = max_layer
+        self.thresh = thresh
+        self.ema_coeff = ema_coeff
+        self.register_buffer('avg_latent', avg_latent)
+    
+    def update(self, next_vec):
+        self.avg_latent.copy_(self.ema_coeff * self.avg_latent + (1-self.ema_coeff) * next_vec)
+    
+    def forward(self, x):
+        interp = torch.lerp(self.avg_latent, x, self.thresh)
+        truncate_indexer = (torch.arange(x.size(1)) < self.max_layer).view(1, -1, 1).to(x.device)
+        return torch.where(truncate_indexer, interp, x)
+
+
+# Upscale/downscale come directly from here: https://github.com/huangzh13/StyleGAN.pytorch/
+class Upscale2d(nn.Module):
+    @staticmethod
+    def upscale2d(x, factor=2, gain=1):
+        assert x.dim() == 4
+        if gain != 1:
+            x = x * gain
+        if factor != 1:
+            shape = x.shape
+            x = x.view(shape[0], shape[1], shape[2], 1, shape[3], 1).expand(-1, -1, -1, factor, -1, factor)
+            x = x.contiguous().view(shape[0], shape[1], factor * shape[2], factor * shape[3])
+        return x
+
+    def __init__(self, factor=2, gain=1):
+        super().__init__()
+        assert isinstance(factor, int) and factor >= 1
+        self.gain = gain
+        self.factor = factor
+
+    def forward(self, x):
+        return self.upscale2d(x, factor=self.factor, gain=self.gain)
+
+# what does gain mean and why is it here???
+class Downscale2d(nn.Module):
+    def __init__(self, factor=2, gain=1):
+        super().__init__()
+        assert isinstance(factor, int) and factor >= 1
+        self.factor = factor
+        self.gain = gain
+        if factor == 2:
+            f = [np.sqrt(gain) / factor] * factor
+            self.blur = BlurLayer(kernel=f, normalize=False, stride=factor)
+        else:
+            self.blur = None
+
+    def forward(self, x):
+        assert x.dim() == 4
+        # 2x2, float32 => downscale using _blur2d().
+        if self.blur is not None and x.dtype == torch.float32:
+            return self.blur(x)
+
+        # Apply gain.
+        if self.gain != 1:
+            x = x * self.gain
+
+        # No-op => early exit.
+        if self.factor == 1:
+            return x
+
+        return F.avg_pool2d(x, self.factor)
+
+
+class BlurLayer(nn.Module):
+    def __init__(self, kernel=None, normalize=True, flip=False, stride=1):
+        super(BlurLayer, self).__init__()
+        if kernel is None:
+            kernel = [1, 2, 1]
+        kernel = torch.tensor(kernel, dtype=torch.float32)
+        kernel = kernel[:, None] * kernel[None, :]
+        kernel = kernel[None, None]
+        if normalize:
+            kernel = kernel / kernel.sum()
+        if flip:
+            kernel = kernel[:, :, ::-1, ::-1]
+        self.register_buffer('kernel', kernel)
+        self.stride = stride
+
+    def forward(self, x):
+        kernel = self.kernel.expand(x.size(1), -1, -1, -1)
+        x = F.conv2d(
+            x,
+            kernel,
+            stride=self.stride,
+            padding=int((self.kernel.size(2) - 1) / 2),
+            groups=x.size(1)
+        )
+        return x
+
+
+
+
+
+'''===================================Progan Blocks==============================================='''
 class FirstGenBlockProGan(nn.Module):
     def __init__(self, noise_size):
         super(FirstGenBlockProGan, self).__init__()
@@ -141,5 +350,78 @@ class DiscrimBlockProGan(nn.Module):
             nn.AvgPool2d(2)
         )
         
+    def forward(self, x):
+        return self.layers(x)
+
+
+'''===================================Stylegan Blocks==============================================='''
+
+class FirstGenBlockStyleGan(nn.Module):
+    def __init__(self, nb_channels, latent_size):
+        super(FirstGenBlockStyleGan, self).__init__()
+        self.nb_channels = nb_channels
+        self.const_input = nn.Parameter(torch.ones(1, nb_channels, 4, 4))
+        self.bias = nn.Parameter(torch.ones(nb_channels))
+
+        self.layers1 = NoiseLReluNormStyle(nb_channels, latent_size)
+        self.conv = EqualizedLRConv2d(nb_channels, nb_channels, 3, padding=1)
+        self.layers2 = NoiseLReluNormStyle(nb_channels, latent_size)
+
+    def forward(self, latents):
+        x = self.const_input.expand(latents.size(0), -1, -1, -1) + self.bias.view(1, -1, 1, 1)
+        x = self.layers1(x, latents[:,0])
+        x = self.conv(x)
+        x = self.layers2(x, latents[:,1])
+        return x
+
+class GenBlockStyleGan(nn.Module):
+    def __init__(self, channels_in, channels_out, latent_size):
+        super(GenBlockStyleGan, self).__init__()
+
+        self.conv1 = EqualizedConv2dUp(channels_in, channels_out, kernel_size=3)
+        self.layers1 = NoiseLReluNormStyle(channels_out, latent_size)
+        self.conv2 = EqualizedLRConv2d(channels_out, channels_out, kernel_size=3, padding=1)
+        self.layers2 = NoiseLReluNormStyle(channels_out, latent_size)
+
+    def forward(self, x, latents):
+        print('Gen block input: ', x.size())
+        x = self.conv1(x)
+        x = self.layers1(x, latents[:,0])
+        x = self.conv2(x)
+        x = self.layers2(x, latents[:,1])
+        print('Gen block output: ', x.size())
+        return x
+        
+class DiscrimLastBlockStyleGan(nn.Module):
+    def __init__(self, in_channels, mid_channels):
+        super(DiscrimLastBlockStyleGan, self).__init__()
+        self.first_layers = nn.Sequential(
+            AddStandardDeviationGrouped(),
+            EqualizedLRConv2d(in_channels + 1, in_channels, kernel_size=3, padding=1),
+            nn.LeakyReLU(.2)
+        )
+        self.second_layers = nn.Sequential(
+            EqualizedLRLinear(in_channels * 4 * 4, mid_channels),
+            nn.LeakyReLU(.2),
+            EqualizedLRLinear(mid_channels, 1)
+        )
+    def forward(self, x):
+        x = self.first_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.second_layers(x)
+        return x
+
+
+class DiscrimBlockStyleGan(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DiscrimBlockStyleGan, self).__init__()
+        self.layers = nn.Sequential(
+            EqualizedLRConv2d(in_channels, in_channels, kernel_size=3, padding=1),
+            nn.LeakyReLU(.2),
+            BlurLayer(),
+            EqualizedConv2dDown(in_channels, out_channels, kernel_size=3),
+            nn.LeakyReLU(.2)
+        )
+
     def forward(self, x):
         return self.layers(x)
--- src/models.py
+++ src/models.py
@@ -5,7 +5,6 @@ from blocks_and_layers import *
 
 
 '''===================================Progan Models==============================================='''
-
 class ProGanGenerator(nn.Module):
     def __init__(self, depth, noise_size):
         super(ProGanGenerator, self).__init__()
@@ -70,3 +69,127 @@ class ProGanDiscriminator(nn.Module):
                 output = layer(output)
             return self.last_block(output)
             
+
+'''===================================StyleGan Models==============================================='''
+class StyleGanMapping(nn.Module):
+    def __init__(self, latent_size_in, latent_size_out, num_layers, num_copies):
+        super(StyleGanMapping, self).__init__()
+        self.latent_size_in = latent_size_in
+        self.latent_size_out = latent_size_out
+        self.num_copies = num_copies
+
+        layers = [PixelNorm()]
+        for i in range(num_layers-1):
+            layers.append(EqualizedLRLinear(latent_size_in, latent_size_in))
+            layers.append(nn.LeakyReLU(.2))
+        layers.append(EqualizedLRLinear(latent_size_in, latent_size_out))
+        layers.append(nn.LeakyReLU(.2))
+        
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layers(x)
+        if self.num_copies:
+            x = x.unsqueeze(1).expand(-1, self.num_copies, -1)
+        return x
+
+def num_feats_per_layer(layer_index, base=8192, decay=1, max_val=512):
+    return min(int(base / (2 **((layer_index+1) * decay))), max_val)
+
+class StyleGanGenSynthesis(nn.Module):
+    def __init__(self, latent_size, max_depth):
+        super(StyleGanGenSynthesis, self).__init__()
+
+        self.max_depth = max_depth
+
+        self.first_layer = FirstGenBlockStyleGan(num_feats_per_layer(0), latent_size)
+        self.rgb_out_layers = nn.ModuleList([EqualizedLRConv2d(num_feats_per_layer(0), 3, (1,1))])
+        self.layers = nn.ModuleList()
+
+        for i in range(1, max_depth):
+            prev_features = num_feats_per_layer(i-1)
+            this_features = num_feats_per_layer(i)
+            self.layers.append(GenBlockStyleGan(prev_features, this_features, latent_size))
+            self.rgb_out_layers.append(EqualizedLRConv2d(this_features, 3, 1))
+
+    def forward(self, latents, depth, alpha):
+        x = self.first_layer(latents[:,:2])
+        if depth > 0:
+            for i, block in enumerate(self.layers[:depth-1]):
+                x = block(x, latents[:, (i+1)*2:(i+1)*2+2])
+
+            prev_depth_out = self.rgb_out_layers[depth-1](F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=False))
+            this_depth_out = self.rgb_out_layers[depth](self.layers[depth-1](x, latents[:,2*depth:2*depth+2]))
+            output = alpha * this_depth_out + (1-alpha) * prev_depth_out
+            return output
+        else:
+            x =  self.rgb_out_layers[0](x)
+            return x
+
+class StyleGanGenerator(nn.Module):
+    def __init__(self, max_depth, latent_size_in, latent_size_map):
+        super(StyleGanGenerator, self).__init__()
+        self.max_depth = max_depth
+        self.mapping_layers = StyleGanMapping(latent_size_in, latent_size_map, 8, max_depth*2)
+        self.synthesis_layers = StyleGanGenSynthesis(latent_size_map, max_depth)
+
+        self.truncation = Truncation(torch.zeros(latent_size_map))
+        self.style_mix_prob = .9
+
+    def forward(self, latents, depth, alpha):
+        mapped_latents = self.mapping_layers(latents)
+        
+
+        if self.training:
+            self.truncation.update(mapped_latents[0,0].detach())
+
+            second_latents = torch.randn(latents.shape).to(latents.device)
+            second_mapped_latents = self.mapping_layers(second_latents)
+            # Have max_depth*2 style blocks
+            layer_idx = torch.arange(self.max_depth * 2).view(1, -1, 1).to(latents.device)
+            curr_layer = 2 * depth + 2
+            cutoff = np.random.randint(1, curr_layer) if np.random.rand() < self.style_mix_prob else curr_layer
+
+            mapped_latents = torch.where(layer_idx < cutoff, mapped_latents, second_mapped_latents)
+
+            mapped_latents = self.truncation(mapped_latents)
+        return self.synthesis_layers(mapped_latents, depth, alpha)
+
+
+class StyleGanDiscriminator(nn.Module):
+    def __init__(self, max_depth, latent_size):
+        super(StyleGanDiscriminator, self).__init__()
+        self.max_depth = max_depth
+
+        self.layers = nn.ModuleList()
+        self.from_rgb = nn.ModuleList()
+        self.last_block = DiscrimLastBlockStyleGan(latent_size, latent_size)
+
+        for i in range(self.max_depth-1, 0, -1):
+            in_channels = num_feats_per_layer(i)
+            out_channels = num_feats_per_layer(i-1)
+            self.layers.append(DiscrimBlockStyleGan(in_channels, out_channels))
+            self.from_rgb.append(EqualizedLRConv2d(3, in_channels, (1,1)))
+
+        self.from_rgb.append(EqualizedLRConv2d(3, num_feats_per_layer(1), (1,1)))
+
+
+    def forward(self, x, depth, alpha):
+        if depth==0:
+            x = self.from_rgb[-1](x)
+            x = self.last_block(x)
+            return x
+        else:
+            smaller_features = self.from_rgb[self.max_depth - depth](nn.AvgPool2d(2)(x))
+            larger_features = self.layers[self.max_depth-depth-1](self.from_rgb[self.max_depth-depth-1](x))
+            x = alpha * larger_features + (1-alpha)*(smaller_features)
+
+            for layer in self.layers[(self.max_depth - depth):]:
+                x = layer(x)
+
+            x = self.last_block(x)
+            return x
+
+
+
+
